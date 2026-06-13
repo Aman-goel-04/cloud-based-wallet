0613fea8357bd9704601527e4e45d1c87060f47d
Aman Goel <amangoel505@gmail.com>
2026-06-13 23:48:26 +0530

    initial frontend setup, added a basic signup page

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,14 @@
+// import React, { useState } from "react";
+
+// function AuthContext({children}: {children: React.ReactNode}) {
+//     const [jwt, setJwt] = useState("");
+//     const [publicKey, setPublicKey] = useState("");
+
+//     const authContext = React.createContext("");
+
+// 	return <authContext.Provider value={{jwt, setJwt, publicKey, setPublicKey}}>
+//         {children};
+//     </authContext.Provider>
+// }
+
+// export default AuthContext;
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { useState } from "react";
+import { instance } from "../api/client";
+
+function Signup() {
+	// const authcontext = useContext(AuthContext);
+	const [formData, setFormData] = useState({ username: "", password: "" });
+    const [publicKey, setPublicKey] = useState("");
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setFormData((prev) => ({ ...prev, [name]: value }));
+	};
+	const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		type responseType = {
+			publicKey: string;
+		};
+		try {
+			const response = await instance.post<responseType>("/signup", {
+				// public key will be retrned
+				username: formData.username,
+				password: formData.password,
+			});
+            setPublicKey(response.data.publicKey);
+		} catch (error) {
+            if (axios.isAxiosError(error)) {
+				alert(error.response?.data?.message ?? "Something went wrong");
+			} else {
+				alert("Something went wrong");
+			}
+		}
+	};
+
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<input
+					name="username"
+					type="text"
+					value={formData.username}
+					onChange={handleChange}
+				/>
+				<input
+					name="password"
+					type="text"
+					value={formData.password}
+					onChange={handleChange}
+				/>
+				<button type="submit"> Sign Up </button>
+			</form>
+            {publicKey && <p>Your wallet address: {publicKey}</p>}
+		</>
+	);
+}
+
+export default Signup;
